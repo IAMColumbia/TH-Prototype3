5c3059fe73170e8e495ca830f47156cc2d007ca5
Fulgore2002 <typryorhitchcock1@gmail.com>
2024-10-15 20:18:21 -0500

    Declared variables for Audio Clips

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,8 @@ public class PlayerController : MonoBehaviour
     private Animator playerAnim;
     public ParticleSystem explosionParticle;
     public ParticleSystem dirtParticle;
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
     public float jumpForce = 10;
     public float gravityModifier = 1;
     public bool isOnGround = true;
